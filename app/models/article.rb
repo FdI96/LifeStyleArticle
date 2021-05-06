5885b2d5d402d8c7c9c2ff45b514c02023bf0f67
FdI96 <federicolopezce@gmail.com>
2021-05-06 16:37:09 -0300

    Add active storage for upload img

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :categories
   has_many :votes
+  has_one_attached :image
 end
