7945a4a2e07d0a060462b89a8c923fc63bdb151a
FdI96 <federicolopezce@gmail.com>
2021-05-13 20:09:11 -0300

    Change test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   describe 'associations' do
-    it { should belong_to(:user) }
+    it { should belong_to(:author) }
     it { should have_and_belong_to_many(:categories) }
     it { should have_many(:votes) }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'associations' do
-    it { should have_many(:votes) }
-    it { should have_many(:articles) }
+    it { should have_many(:votes).dependents(:destroy) }
+    it { should have_many(:articles).dependents(:destroy) }
   end
 end
