6871b5ee40fede6d15092aadbd15982104f3fd13
FdI96 <federicolopezce@gmail.com>
2021-05-13 13:57:39 -0300

    Fix all N+1 problems

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :text, presence: true
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :categories
   has_many :votes, dependent: :destroy
