082d5502f349b7260bcdb1988dfe3b7142c999ba
FdI96 <federicolopezce@gmail.com>
2021-05-15 19:14:03 -0300

    Add validation for min and max for title and text fields from articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ApplicationRecord
   validates :image, presence: true
   validates :title, presence: true
   validates :text, presence: true
+  validates_length_of :title, minimum: 3, maximum: 10, allow_blank: true
+  validates_length_of :foo, minimum: 1, maximum: 120, allow_blank: true
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :categories, class_name: 'Category'
   has_many :votes, dependent: :destroy
