c0d74e2a6970814cbd8b278e862eb5e59a5f978c
FdI96 <federicolopezce@gmail.com>
2021-05-15 19:03:56 -0300

    Modify sorted articles by most recent, delete all the comments in rspec, Modify relation to has_many categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
   belongs_to :author, class_name: 'User', foreign_key: :author_id
-  has_and_belongs_to_many :categories, class_name: 'Category'
+  has_many :categories, class_name: 'Category'
   has_many :votes, dependent: :destroy
   has_one_attached :image
 end
