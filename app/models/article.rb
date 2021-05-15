c203ca4365a041d04ecddf1f17ff2ceef38cdb6a
FdI96 <federicolopezce@gmail.com>
2021-05-15 19:20:27 -0300

    Move footer to the index article view

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
   validates_length_of :title, minimum: 3, maximum: 10, allow_blank: true
-  validates_length_of :foo, minimum: 1, maximum: 120, allow_blank: true
+  validates_length_of :text, minimum: 1, maximum: 120, allow_blank: true
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :categories, class_name: 'Category'
   has_many :votes, dependent: :destroy
