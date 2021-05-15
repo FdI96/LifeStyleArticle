107457181a4a12bd39412c59b31f2c6c2ec01d30
FdI96 <federicolopezce@gmail.com>
2021-05-15 19:32:34 -0300

    Fix rubocop errors

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,16 +1,16 @@
 module ArticlesHelper
   def display_errors(article)
-    if article.errors.any?
-      html = ''
-      html = "<div id='error_explanation'>"
-      html = content_tag(:h2, "#{pluralize(article.errors.count, 'error')} prohibited this article from being saved:")
-      html = '<ul>'
-      article.errors.each do |error|
-        html = content_tag(:li, error.full_message)
-      end
-      html = '</ul>'
-      html = '</div>'
-      html.html_safe
+    return unless article.errors.any?
+
+    html = ''
+    html = "<div id='error_explanation'>"
+    html = content_tag(:h2, "#{pluralize(article.errors.count, 'error')} prohibited this article from being saved:")
+    html = '<ul>'
+    article.errors.each do |error|
+      html = content_tag(:li, error.full_message)
     end
+    html = '</ul>'
+    html = '</div>'
+    html.html_safe
   end
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,16 +1,16 @@
 module CategoriesHelper
   def display_errors(category)
-    if category.errors.any?
-      html = ''
-      html = "<div id='error_explanation'>"
-      html = content_tag(:h2, "#{pluralize(category.errors.count, 'error')} prohibited this article from being saved:")
-      html = '<ul>'
-      category.errors.each do |error|
-        html = content_tag(:li, error.full_message)
-      end
-      html = '</ul>'
-      html = '</div>'
-      html.html_safe
+    return unless category.errors.any?
+
+    html = ''
+    html = "<div id='error_explanation'>"
+    html = content_tag(:h2, "#{pluralize(category.errors.count, 'error')} prohibited this article from being saved:")
+    html = '<ul>'
+    category.errors.each do |error|
+      html = content_tag(:li, error.full_message)
     end
+    html = '</ul>'
+    html = '</div>'
+    html.html_safe
   end
 end
